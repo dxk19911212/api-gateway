71c839b8d9876fa86b20eba830067d4bbe345b68
dxk19911212 <daixiaokang@xiaoweiba.com.cn>
2017-12-21 14:19:57 +0800

    修改

--- src/main/java/com/dxk/Application.java
+++ src/main/java/com/dxk/Application.java
@@ -7,6 +7,13 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 @EnableZuulProxy
 @SpringCloudApplication
 public class Application {
@@ -26,5 +33,24 @@ public class Application {
 //				"(?<name>^.+)-(?<version>v.+$)",
 //				"${version}/${name}");
 //	}
+
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.dxk.web"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("api-gateway interface")
+				.description("api-gateway接口文档")
+				.termsOfServiceUrl("localhost")
+				.version("1.0")
+				.build();
+	}
 
 }
